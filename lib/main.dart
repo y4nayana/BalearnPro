582bdd9ab7bd299b8d5f99bface53aa103510cc2
Daffathoriq <daffathoriq17@gmail.com>
2025-01-08 02:39:18 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,29 @@
-
-// main.dart
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
-import 'screens/splash_screen.dart';
-import 'screens/auth/login_screen.dart';
-import 'screens/home/home_screen.dart';
+import 'screens/splash_screen.dart';    // SplashScreen harus ada
+import 'screens/home/home_screen.dart';  // Impor HomeScreen
+import 'screens/auth/login_screen.dart'; // Impor LoginScreen
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();  // Inisialisasi Firebase
+  runApp(MyApp());  // Jalankan aplikasi setelah Firebase diinisialisasi
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-      ],
+    return ChangeNotifierProvider(
+      create: (context) => AuthProvider(),
       child: MaterialApp(
-        debugShowCheckedModeBanner: false,
         title: 'Balearnpro',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
         initialRoute: '/',
         routes: {
-          '/': (context) => SplashScreen(),
-          '/login': (context) => LoginScreen(),
-          '/home': (context) => HomeScreen(),
+          '/': (context) => SplashScreen(),  // SplashScreen yang pertama
+          '/home': (context) => HomeScreen(),  // Halaman Home
+          '/login': (context) => LoginScreen(), // Halaman Login
         },
       ),
     );
